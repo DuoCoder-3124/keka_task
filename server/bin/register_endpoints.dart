32cbe19e61d957b114a5c6bcba5c6fe1d975d755
Ayushman <ayushman.g@elaunchinfotech.in>
2024-03-11 16:54:16 +0530

    Register API Done

---
+++ server/bin/register_endpoints.dart
@@ -0,0 +1,40 @@
+part of 'server.dart';
+
+Future<Response> _register(Request request) async {
+  String body = await request.readAsString();
+  Map<String, dynamic> data = jsonDecode(body);
+
+  Map<String, dynamic> rawBody = {
+    'firstName': data['firstName'],
+    'secondName': data['secondName'],
+    'middleName': data['middleName'],
+    'email': data['email'],
+    'password': data['password'],
+    'DOB': data['DOB'],
+    'gender': data['gender'],
+    'department': data['department'],
+    'reportedBy': data['reportedBy'],
+    'jobTitle': data['jobTitle'],
+    'phoneNumber': data['phoneNumber'],
+  };
+
+  List<String> nullFields = [];
+
+  rawBody.forEach((key, value) {
+    if (value == null) {
+      nullFields.add(key);
+    }
+  });
+
+  if (nullFields.isEmpty) {
+    DbCollection? collection = db?.collection("user");
+    rawBody.addAll({'_id': _docId});
+    rawBody.update('password', (value) => _encryptPassword(rawBody['password']));
+    collection?.insertOne(rawBody);
+    return Response.ok(jsonEncode({"message": "Employee inserted Successfully."}));
+  }
+  return Response.forbidden(
+    jsonEncode({"null fields": nullFields}),
+  );
+  return Response.badRequest(body: "Body should not be empty.");
+}
--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,33 +1,44 @@
+import 'dart:convert';
 import 'dart:io';
-
+import 'package:mongo_dart/mongo_dart.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:crypto/crypto.dart' as crypto;
+
+part 'register_endpoints.dart';
 
 // Configure routes.
 final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+  ..get('/', (req) => Response.ok("Hello Keka!!!"))
+  ..post('/registerEmployee', _register);
 
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, Server!\n');
-}
-
-Response _echoHandler(Request request) {
-  final message = request.params['message'];
-  return Response.ok('$message\n');
-}
+///Instance for database.
+Db? db;
 
 void main(List<String> args) async {
+  //Connection to Database.
+  db = await Db.create("mongodb+srv://ayushmang:PUPzdiw2Y9tLdMPH@keka.cikkk4s.mongodb.net/Keka");
+  db?.open();
+
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  final handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(handler, ip, port);
+  final server = await serve(handler, ip, 3000);
   print('Server listening on port ${server.port}');
+}
+
+///Gets id for every document that needs to be inserted in MongoDB.
+String get _docId => ObjectId().oid;
+
+
+String _encryptPassword(String password) {
+  final bytes = utf8.encode(password);
+  final digest = crypto.sha256.convert(bytes);
+  return digest.toString();
 }
