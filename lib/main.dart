be2d1df223ec3d68af107d15f5feb94be4a51a0b
nehal shinde <duocoder3124@gmail.com>
2024-03-11 14:48:34 +0530

    keka_task

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // initialRoute: BottomNavBarView.routeName,
-      initialRoute: ProfileView.routeName,
+      initialRoute: HomeView.routeName,
       routes: route,
       // home: Temp(),
     );
