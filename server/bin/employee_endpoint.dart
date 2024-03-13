b97998fcf07662b940d68f33f591752e32115a6f
Ayushman <ayushman.g@elaunchinfotech.in>
2024-03-13 17:29:19 +0530

    Update put API done.

---
+++ server/bin/employee_endpoint.dart
@@ -0,0 +1,113 @@
+part of 'server.dart';
+
+///Gets user data on the basis of userId.
+Future<Response> _getEmployeeById(Request request) async {
+  Map<String, dynamic> params = request.url.queryParameters;
+
+  if (params.isNotEmpty) {
+    String userId = params['userId'];
+
+    DbCollection? collection = db?.collection("user");
+    Map<String, dynamic>? userData = await collection?.findOne(
+      where.eq('_id', userId),
+    );
+    if (userData!.isNotEmpty) {
+      return Response.ok(
+        jsonEncode(
+          {
+            "message": "Found user",
+            "userData": userData,
+          },
+        ),
+      );
+    }
+    return Response.notFound(
+      jsonEncode(
+        {
+          "message": "User cannot be found",
+        },
+      ),
+    );
+  }
+  return Response.badRequest(body: "Parameter userId should not be empty.");
+}
+
+///Gets all user data.
+Future<Response> _getEmployee(Request request) async {
+  DbCollection? collection = db?.collection("user");
+  var docs = await collection?.find().toList();
+  return Response.ok(
+    jsonEncode(docs),
+  );
+}
+
+///Put API to update Employee.
+Future<Response> _updateEmployee(Request request) async {
+  String body = await request.readAsString();
+  Map<String, dynamic> data = jsonDecode(body);
+
+  Map<String, dynamic> rawBody = {
+    'userId': data['userId'],
+    'firstName': data['firstName'],
+    'secondName': data['secondName'],
+    'middleName': data['middleName'],
+    'DOB': data['DOB'],
+    'gender': data['gender'],
+    'department': data['department'],
+    'reportedBy': data['reportedBy'],
+    'jobTitle': data['jobTitle'],
+    'phoneNumber': data['phoneNumber'],
+  };
+
+  List<String> nullFields = [];
+
+  rawBody.forEach((key, value) {
+    if (value == null) {
+      nullFields.add(key);
+    }
+  });
+
+  if (nullFields.isEmpty) {
+    DbCollection? collection = db?.collection("user");
+    Map<String, dynamic>? userData = await collection?.findOne(
+      where.eq(
+        '_id',
+        rawBody["userId"],
+      ),
+    );
+    if (userData == null) {
+      collection?.insertOne(rawBody);
+      await collection?.updateOne(
+        await collection.findOne(
+          where.eq('_id', rawBody['userId']),
+        ),
+        {
+          '\$set': {
+            'firstName': rawBody['firstName'],
+            'secondName': rawBody['secondName'],
+            'middleName': rawBody['middleName'],
+            'DOB': rawBody['DOB'],
+            'gender': rawBody['gender'],
+            'department': rawBody['department'],
+            'reportedBy': rawBody['reportedBy'],
+            'jobTitle': rawBody['jobTitle'],
+            'phoneNumber': rawBody['phoneNumber'],
+          }
+        },
+      );
+      return Response.ok(
+        jsonEncode(
+          {"message": "Employee updated Successfully."},
+        ),
+      );
+    }
+    return Response.forbidden(
+      jsonEncode(
+        {"message": "User Not Found"},
+      ),
+    );
+  }
+  return Response.forbidden(
+    jsonEncode({"null fields": nullFields}),
+  );
+}
